782e473cfa486e15a8be64c3e4febf566e05e507
Denis Dmitriev <dmitrievdv242@gmail.com>
2022-02-23 21:26:30 +0300

    wordscore

--- semantle.jl
+++ semantle.jl
@@ -35,14 +35,20 @@ function findoppositepairs(wv :: WordVectors, threshold :: Real; n = 10)
     opposite_pairs
 end
 
-# function wordscore(wv :: WordVectors, word; n = 100)
-#     similar_words = cosine_similar_words(wv, word, n)
-#     min_similarity = 1.0
-#     for w in similar_words
-#         sim = similarity(wv, w, word)
-#         if sim < min_similarity
-#             min_similarity = sim
-#         end
-#     end
-
-# end
+function wordscore(wv :: WordVectors, word; n = 10)
+    similar_words = cosine_similar_words(wv, word, n)
+    score = 0.0
+    sum_sim = 0.0
+    for w in similar_words
+        sim = similarity(wv, w, word)
+        similar_to_similar = cosine_similar_words(wv, word, n)
+        mean_sim = 0.0
+        for ww in similar_to_similar
+            sim_sim = similarity(wv, ww, word)
+            mean_sim += sim_sim/n
+        end
+        score += sim*mean_sim
+        sum_sim += sim
+    end
+    return score/sum_sim
+end
