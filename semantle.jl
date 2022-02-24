f786d494df42367b08472fed51c8d576018d3fde
Denis Dmitriev <dmitrievdv242@gmail.com>
2022-02-24 08:30:42 +0300

    guess

--- semantle.jl
+++ semantle.jl
@@ -52,3 +52,41 @@ function wordscore(wv :: WordVectors, word; n = 10)
     end
     return score/sum_sim
 end
+
+function semantlegeometricguess(wv, guesses, similarities; n = 10, mean = :geometric, rnd_threshold = 0.3, rnd_prob = 0.9)
+    max_lg_mean_sim = -2.0
+    best_word = ""
+    best_indices = sortperm(similarities)
+    n_guesses = length(guesses)
+    start_guess = if length(guesses) > n
+        n_guesses - n + 1
+    else
+        1
+    end
+    sum_sim = sum(similarities[best_indices[start_guess:n_guesses]])
+    println(similarities[best_indices[start_guess:n_guesses]])
+    for w in wv.vocab
+        if w in guesses
+            continue
+        end
+        lg_mean_sim = 0.0
+        for i in start_guess:n_guesses
+            ind = best_indices[i]
+            g = guesses[ind]
+            s = similarities[ind]
+            sim = abs(similarity(wv, w, g))
+            lg_mean_sim += log(sim)*s/sum_sim
+        end
+        if lg_mean_sim > max_lg_mean_sim
+            max_lg_mean_sim = lg_mean_sim
+            best_word = w
+        end
+    end
+    best_word = if (rand()*rnd_threshold > sum_sim/n) | (rand() > rnd_prob) 
+        println("rand:")
+        rand(wv.vocab)
+    else
+        println("best:")
+        best_word
+    end
+end
