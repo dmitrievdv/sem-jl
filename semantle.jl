dff6d8378a2953647d91c9da6c49be0f8f16c58b
dmitrievdv <dmitrievdv242@gmail.com>
2022-02-24 16:05:12 +0300

    semantle game added

--- semantle.jl
+++ semantle.jl
@@ -53,9 +53,7 @@ function wordscore(wv :: WordVectors, word; n = 10)
     return score/sum_sim
 end
 
-function semantlegeometricguess(wv, guesses, similarities; n = 10, mean = :geometric, rnd_threshold = 0.3, rnd_prob = 0.9)
-    max_lg_mean_sim = -2.0
-    best_word = ""
+function semantlegeometricguess(wv, guesses, similarities; n = 10, rnd_prob = 0.3, worst_rare = 1.0)
     best_indices = sortperm(similarities)
     n_guesses = length(guesses)
     start_guess = if length(guesses) > n
@@ -63,30 +61,83 @@ function semantlegeometricguess(wv, guesses, similarities; n = 10, mean = :geome
     else
         1
     end
-    sum_sim = sum(similarities[best_indices[start_guess:n_guesses]])
-    println(similarities[best_indices[start_guess:n_guesses]])
-    for w in wv.vocab
-        if w in guesses
-            continue
+    rnd = rand()
+    n_counts = min(n, n_guesses)
+    sum_sim_best = sum(abs.(similarities[best_indices[start_guess:n_guesses]]))
+    sum_sim_worst = sum(1 .- abs.(similarities[best_indices[1:n_counts]]))
+    
+    if rnd < rnd_prob
+        w = guesses[1]
+        while w in guesses
+            w = rand(wv.vocab)
         end
-        lg_mean_sim = 0.0
-        for i in start_guess:n_guesses
-            ind = best_indices[i]
-            g = guesses[ind]
-            s = similarities[ind]
-            sim = abs(similarity(wv, w, g))
-            lg_mean_sim += log(sim)*s/sum_sim
-        end
-        if lg_mean_sim > max_lg_mean_sim
-            max_lg_mean_sim = lg_mean_sim
-            best_word = w
-        end
-    end
-    best_word = if (rand()*rnd_threshold > sum_sim/n) | (rand() > rnd_prob) 
-        println("rand:")
-        rand(wv.vocab)
+        println("rand: $w")
+        return w
     else
-        println("best:")
-        best_word
+        from_best_word = ""
+        from_worst_word = ""
+        max_lg_mean_sim = -2.0
+        min_lg_mean_sim = 0.0
+        for w in wv.vocab
+            if w in guesses
+                continue
+            end
+            lg_mean_sim = 0.0
+            for i in start_guess:n_guesses
+                ind = best_indices[i]
+                g = guesses[ind]
+                s = similarities[ind]
+                sim = abs(similarity(wv, w, g))
+                lg_mean_sim += log(sim)*s/sum_sim_best
+            end
+            if lg_mean_sim > max_lg_mean_sim
+                max_lg_mean_sim = lg_mean_sim
+                from_best_word = w
+            end
+            lg_mean_sim = 0.0
+            for i in 1:n_counts
+                ind = best_indices[i]
+                g = guesses[ind]
+                s = similarities[ind]
+                sim = abs(similarity(wv, w, g))
+                lg_mean_sim += log(sim)*(1-abs(s))/sum_sim_worst
+            end
+            if lg_mean_sim < min_lg_mean_sim
+                min_lg_mean_sim = lg_mean_sim
+                from_worst_word = w
+            end
+        end
+        if rnd < rnd_prob + (1-rnd_prob)*(1 - sum_sim_best/n_counts)/worst_rare
+            println("worst: $from_worst_word")
+            return from_worst_word
+        else
+            println("best: $from_best_word")
+            return from_best_word
+        end
     end
+end
+
+function semantlegame(wv :: WordVectors, word :: AbstractString; rnd_prob = 0.3, worst_rare = 1.0)
+    guesses = rand(wv.vocab, 5)
+    sims = zeros(5)
+    for i = 1:5
+        w = guesses[i]
+        s = similarity(wv, w, word)
+        sims[i] = s
+    end
+    if word in guesses
+        println("Randomly guessed!")
+        return guesses, sims
+    end
+    n_gueses = 5
+    while guesses[end] != word
+        n_gueses += 1
+        print("Guess #$n_gueses from ")
+        guess = semantlegeometricguess(wv, guesses, sims, rnd_prob = rnd_prob, worst_rare = worst_rare)
+        s = abs(similarity(wv, guess, word))
+        println("  similarity score $(100*s)")
+        push!(guesses, guess); push!(sims, s)
+    end
+    println("Found after $n_gueses guesses!")
+    return guesses, sims
 end
